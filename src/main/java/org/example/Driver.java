61b9b8d06458d552debd53f6e7170fe302c99588
Josh Hodges <sick_libra@hotmail.com>
2024-05-10 23:57:43 -0400

    second commit all files funcitoning as expected.

--- src/main/java/org/example/Driver.java
+++ src/main/java/org/example/Driver.java
@@ -1,8 +1,18 @@
 package org.example;
+import  static org.example.Game.serializeToCSV;
+import static org.example.Game.deserializeFromCSV;
 
 public class Driver {
     public static void main(String[] args) {
+        String file= "gamelog.csv";
 
+        Game outGame= new Game("Legend of Zelda", "Nintendo",1, "Cartridge");
+        serializeToCSV(outGame, file);
+        Game ingame= deserializeFromCSV(file);
+        System.out.println(outGame.equals(ingame));
+        System.out.println(outGame.csvStr());
+        System.out.println(ingame.csvStr());
+        System.out.println("Do they match?");
 
     }
 }
--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -1,8 +1,16 @@
 package org.example;
 
 import java.util.Objects;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Scanner;
+import java.util.List;
 
-public class Game implements VideoGame, Comparable {
+public class Game implements VideoGame, Comparable, Serializable {
     String title;
     String system;
     int numPlayers;
@@ -23,6 +31,18 @@ public class Game implements VideoGame, Comparable {
         this.numPlayers = numPlayers;
         this.format = format;
     }
+    //Mutators
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public void setSystem(String system){
+        this.system = system;
+    }
+    public void setNumPlayers(int numPlayers){
+        this.numPlayers = numPlayers;
+    }
+    public void setFormat(String format){this.format = format;}
+
     //String to write to csv
     public String csvStr() {
         String strplayers = String.valueOf(numPlayers);
@@ -61,5 +81,32 @@ public class Game implements VideoGame, Comparable {
     @Override
     public int hashCode() {
         return Objects.hash(title, numPlayers);
+    }
+    public static void serializeToCSV(Game game,String file){
+        Path filePath = Paths.get(file);
+        try{
+            Files.writeString(filePath, game.csvStr());
+        }
+        catch (IOException e){
+            System.out.println("Something went wrong.");
+            System.out.println(e.getMessage());
+        }
+    }
+    public static Game deserializeFromCSV(String file){
+        Game gameFromFile = new Game();
+        try {
+            String str = Files.readString(Paths.get(file), StandardCharsets.UTF_8);
+
+            String[] line = str.split(",");
+            gameFromFile.setTitle(line[0]);
+            gameFromFile.setSystem(line[1]);
+            gameFromFile.setNumPlayers(Integer.parseInt(line[2]));
+            gameFromFile.setFormat(line[3]);
+
+        }
+        catch(IOException e){
+            System.out.println("Please check the file name.");
+        }
+        return gameFromFile;
     }
 }
